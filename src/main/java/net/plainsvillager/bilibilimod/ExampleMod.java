45457c87275be6c73f3869266e54cfd34a04af8a
PlainsVillager <littlecuckoo@outlook.com>
2022-04-28 10:45:24 +0800

    updated

--- src/main/java/net/plainsvillager/bilibilimod/ExampleMod.java
+++ src/main/java/net/plainsvillager/bilibilimod/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package net.plainsvillager.bilibilimod;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/net/plainsvillager/bilibilimod/mixin/ExampleMixin.java
+++ src/main/java/net/plainsvillager/bilibilimod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.plainsvillager.bilibilimod.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.plainsvillager.bilibilimod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
