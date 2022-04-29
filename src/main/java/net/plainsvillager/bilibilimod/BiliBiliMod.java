684f9d8213f007fc6cb84316976d0614216d818a
PlainsVillager <littlecuckoo@outlook.com>
2022-04-29 09:38:31 +0800

    deveoping a new futrue (testing)

--- src/main/java/net/plainsvillager/bilibilimod/BiliBiliMod.java
+++ src/main/java/net/plainsvillager/bilibilimod/BiliBiliMod.java
@@ -1,14 +1,17 @@
 package net.plainsvillager.bilibilimod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class BiliBiliMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Item =
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/plainsvillager/bilibilimod/mixin/ExampleMixin.java
+++ src/main/java/net/plainsvillager/bilibilimod/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
 package net.plainsvillager.bilibilimod.mixin;
 
-import net.plainsvillager.bilibilimod.ExampleMod;
+import net.plainsvillager.bilibilimod.BiliBiliMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		BiliBiliMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
